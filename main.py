be695daf9aa7630921c6898d21d7c034f15fcaf3
Rares Patrascu <rares.patrascu@countthings.com>
2022-08-29 12:22:20 +0300

    Converting the yolo info and dump it into coco formatted json objects, also added a gitignore file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from queue import Empty
 
 from create_annotations import (
     create_image_annotation,
@@ -11,6 +12,7 @@ import cv2
 import argparse
 import json
 import numpy as np
+import json
 
 Data = {
         "train":{},
@@ -29,7 +31,7 @@ def get_args():
     parser.add_argument(
         "--output",
         type=str,
-        help="Name the output location for the coco dataset format",
+        help="Output location for the coco dataset format",
     )
     args = parser.parse_args()
     return args
@@ -42,8 +44,16 @@ def main(opt):
     classes = get_classes_list(opt)
     data = get_data(opt,Data,classes)
 
-    path = Path("parentdirectory/annotations")
-    path.mkdir(parents=True, exist_ok=True)
+    annot_path = opt.output+"/annotations"
+    Path(annot_path).mkdir(parents=True, exist_ok=True)
+
+
+    for key in data:
+        if data[key]:
+            file_name = annot_path + "/instances_" + str(key) + ".json"
+            with open(file_name, "w") as outfile:
+                json.dump(data[key], outfile, indent=4)
+
 
     print("hehe")
 
